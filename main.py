7512cf29bf07f5280e29a1529bc154e9342158a1
stevenjlance <steven.j.lance@gmail.com>
2025-03-17 17:34:09 -0400

    solutions added

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from api import *
 
-# Test your code here
+# Test outputs
+client = ReqresAPIClient()
+
+# PUT Demonstration
+print("\n--- PUT Requests ---")
+updated_user = client.update_user(2, "Alice Johnson", "Designer")
+print("Updated User:")
+print(updated_user)
+
+# DELETE Demonstration
+print("\n--- DELETE Requests ---")
+deleted = client.delete_user(2)
+print("User Deleted:", deleted)
