19e916ee0db3251e480e3736e26998ce95287b8c
stevenjlance <steven.j.lance@gmail.com>
2025-04-03 17:08:43 -0400

    updated unit tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,4 @@
-from api import *
+from api import ReqresAPIClient
 from unittest.mock import patch
 
 # Test class initialization
@@ -57,3 +57,36 @@ def test_create_user_bad_status():
         mock_post.return_value.status_code = 400
         new_user = client.create_user("Alice Smith", "Developer")
         assert new_user is None
+
+def test_update_user():
+    '''Test the create_user method of the ReqresAPIClient class'''
+    client = ReqresAPIClient()
+    new_user = client.update_user(3, "Alice Smith", "Developer")
+    assert new_user is not None
+    assert new_user["name"] == "Alice Smith"
+    assert new_user["job"] == "Developer"
+
+# Test update_user for status code that is not 200
+@patch('api.requests.put')
+def test_update_user_error(mock_put):
+    '''Test the create_user method of the ReqresAPIClient class with an error response'''
+    mock_put.return_value.status_code = 404
+    client = ReqresAPIClient()
+    new_user = client.update_user(3, "Alice Smith", "Developer")
+    assert new_user is None
+
+# Test delete_user
+def test_delete_user():
+    '''Test the delete_user method of the ReqresAPIClient class'''
+    client = ReqresAPIClient()
+    deleted = client.delete_user(3)
+    assert deleted is True
+
+# Test delete_user for status code that is not 204
+@patch('api.requests.delete')
+def test_delete_user_error(mock_delete):
+    '''Test the delete_user method of the ReqresAPIClient class with an error response'''
+    mock_delete.return_value.status_code = 404
+    client = ReqresAPIClient()
+    deleted = client.delete_user(3)
+    assert deleted is False
